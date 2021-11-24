06e05943f4d6391f07b7994424b4963b71a45749
Naser Abdulkadir <naserabdulkadir0@gmail.com>
2021-11-24 18:27:26 -0500

    saving here because im gonna try something. trying to get the data from this data.json file but for some reason it keeps saying the get request failed. I dont think its my file path but i'm gonna try and make sure.

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 "use strict";
 
 const nestedGrid = document.querySelector(".nested-grid");
-
 console.log(nestedGrid);
 
-function createPlay() {
-  const play = document.createElement("div");
-  console.log(play);
+let url = "data.json";
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+function heroImg() {
+  const heroImg = document.createElement("div");
+  heroImg.classList.add("hero-img");
+  return heroImg;
 }
-createPlay();
+
+function createContent() {
+  const content = document.createElement("div");
+  content.classList.add("content");
+  return content;
+}
+
+function createDiv() {
+  const div = document.createElement("div");
+  div.classList.add("row-sizing");
+
+  let heroImage = heroImg();
+  let content = createContent();
+
+  div.appendChild(heroImage);
+  div.appendChild(content);
+  nestedGrid.appendChild(div);
+}
+createDiv();
