748f735573836c242f4a42bfcd99a84148e43c2d
Naser Abdulkadir <naserabdulkadir0@gmail.com>
2021-11-24 14:41:02 -0500

    was able to align the hero img div properly using position absolute. now i need to gameplan how im gonna create the rest of them.

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const nestedGrid = document.querySelector(".nested-grid");
+
+console.log(nestedGrid);
+
+function createPlay() {
+  const play = document.createElement("div");
+  console.log(play);
+}
+createPlay();
